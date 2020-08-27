e673e6d4102987cee9616ebbf9d0b6e3bd1f4125
Vasil <vaskovasilev94@yahoo.com>
2020-08-27 22:13:51 +0200

    initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module scrapeWatch
 
 go 1.14
+
+require (
+	cloud.google.com/go/firestore v1.2.0
+	github.com/sp0x/torrentd v0.3.10
+	github.com/spf13/viper v1.7.1
+	google.golang.org/api v0.28.0
+)
--- statusCollector.go
+++ statusCollector.go
@@ -1,1 +1,88 @@
 package scrapeWatch
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/spf13/viper"
+	"google.golang.org/api/option"
+	"log"
+	"os"
+)
+
+func BindConfig() {
+	viper.AutomaticEnv()
+	_ = viper.BindEnv("firebase_project")
+	_ = viper.BindEnv("firebase_credentials_file")
+}
+
+const schemeTopic = "scrapescheme"
+const schemeErrorTopic = "scheme.topic"
+
+type FirebaseConfig struct {
+	Project     string
+	Credentials string
+}
+
+func GetFirebaseConfig() (*FirebaseConfig, error) {
+	project := viper.GetString("firebase_project")
+	creds := viper.GetString("firebase_credentials_file")
+	if creds == "" {
+		return nil, errors.New("no firebase credentials found")
+	}
+	return &FirebaseConfig{
+		Project:     project,
+		Credentials: creds,
+	}, nil
+}
+
+func NewFirebaseFromEnv() (*firestore.Client, error) {
+	c, err := GetFirebaseConfig()
+	if err != nil {
+		return nil, err
+	}
+	return NewFirebase(c.Project, c.Credentials)
+}
+
+func NewFirebase(projectId string, credentialsFile string) (*firestore.Client, error) {
+	ctx := context.Background()
+	var options []option.ClientOption
+	if credentialsFile != "" {
+		options = append(options, option.WithCredentialsFile(credentialsFile))
+	}
+	// credentials file option is optional, by default it will use GOOGLE_APPLICATION_CREDENTIALS
+	// environment variable, this is a default method to connect to Google services
+	client, err := firestore.NewClient(ctx, projectId, options...)
+	return client, err
+}
+
+// PubSubMessage is the payload of a Pub/Sub event.
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+var initialized = false
+var firebase *firestore.Client
+
+func initialize() {
+	if initialized {
+		return
+	}
+	initialized = true
+	fb, err := NewFirebaseFromEnv()
+	if err != nil {
+		fmt.Printf("error initializing firestore: %v", err)
+		os.Exit(1)
+	}
+	firebase = fb
+
+}
+
+// HelloPubSub consumes a Pub/Sub message.
+func NonErrorStatusReceived(ctx context.Context, m PubSubMessage) error {
+	name := string(m.Data) // Automatically decoded from base64.
+	initialize()
+	log.Printf(name)
+	return nil
+}
